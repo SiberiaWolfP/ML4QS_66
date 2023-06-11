56d085f03e9c7dc47a631c307c9fc389a472d0e7
zhiheng-yang <young-z-h@outlook.com>
2023-06-11 13:10:13 +0200

    some data

--- src/filter/filter.py
+++ src/filter/filter.py
@@ -3,9 +3,13 @@ import pandas as pd
 import glob
 import os
 
+# slow!
+
 raw_file = '../../datasets/intermediate/raw.csv'  # gets current directory
 data_folder = '../../datasets/activities/after_impute_missing_values/'
 result_folder = '../../datasets/activities/after_filter/'
+if not os.path.exists(result_folder):
+    os.makedirs(result_folder)
 
 filter_columns = ['Accelerometer z', 'Accelerometer y', 'Accelerometer x',
                    # 'Gravity z', 'Gravity y', 'Gravity x',
@@ -16,16 +20,8 @@ filter_columns = ['Accelerometer z', 'Accelerometer y', 'Accelerometer x',
                   ]
 
 csv_files = glob.glob(data_folder + '/*.csv')
-
 for csv_file in csv_files:
     print(os.path.basename(csv_file))
     dataset = pd.read_csv(csv_file)
     dataset = KalmanFilters().apply_kalman_filter(dataset, filter_columns)
-    dataset = dataset.dropna() # 比如最开始的缺失值
     dataset.to_csv(result_folder + os.path.basename(csv_file))
-
-    # break
-
-
-
-
