ca94b2d84b5b980b31592c3362440ea684d921cc
zhiheng-yang <young-z-h@outlook.com>
2023-06-14 13:22:53 +0200

    mps accelerated pytorch on ARM

--- util/common.py
+++ util/common.py
@@ -2,3 +2,8 @@ import torch
 GPU = False
 if torch.cuda.is_available():
     GPU = True
+
+if torch.backends.mps.is_available():
+    mps_device = torch.device("mps")
+    x = torch.ones(1, device=mps_device)
+    # print (x)
