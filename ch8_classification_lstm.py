11207d79bde170e34d6553143df06b3440955536
zhiheng-yang <young-z-h@outlook.com>
2023-06-24 00:50:40 +0200

    lstm full splited dataset

--- ch8_classification_lstm.py
+++ ch8_classification_lstm.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-from sklearn.model_selection import train_test_split
-from keras.preprocessing.sequence import pad_sequences
 from keras.layers import LSTM, Embedding, Dense
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
@@ -11,51 +9,43 @@ import seaborn as sns
 import matplotlib as mpl
 
 target = 'class'
-sensor_data = pd.read_csv('/Users/young/Downloads/ml4qs_codes/ML4QS_66/datasets/intermediate/chapter5_result_class_time.csv')
+# sensor_data_train = pd.read_csv('/Users/young/Downloads/ml4qs_codes/ML4QS_66/datasets/intermediate/bugfixed/chapter4_result_train_full.csv')
+# sensor_data_test = pd.read_csv('/Users/young/Downloads/ml4qs_codes/ML4QS_66/datasets/intermediate/bugfixed/chapter4_result_test_full.csv')
+sensor_data_train = pd.read_csv('/Users/young/Downloads/ml4qs_codes/ML4QS_66/datasets/intermediate/bugfixed/chapter4_result_train.csv')
+sensor_data_test = pd.read_csv('/Users/young/Downloads/ml4qs_codes/ML4QS_66/datasets/intermediate/bugfixed/chapter4_result_test.csv')
 
-print(sensor_data.columns)
-sensor_data = sensor_data[['time','gra_y_min', 'gyr_x_diff_2_temp_std_ws_300', 'mag_z_temp_std_ws_300',
-                              'gra_y_median_temp_std_ws_300', 'mag_y_diff_2_temp_std_ws_300',
-                              'gra_y_autocorr_mean_temp_std_ws_300', 'ori_qz_fft_min_temp_mean_ws_300',
-                              'acc_z_fft_std_temp_std_ws_300', 'mag_z_max_temp_mean_ws_300',
-                              'gra_y_autocorr_median_temp_std_ws_300', 'acc_y_fft_max', 'acc_y_std',
-                              'mag_x_autocorr_max_temp_std_ws_300', 'gra_y', 'mag_x_skew_temp_std_ws_300',
-                              'gra_y_median', 'gyr_y_autocorr_median_temp_mean_ws_300', 'gyr_z_std_temp_mean_ws_300',
-                              'mic_dBFS', 'ori_qz_min_temp_mean_ws_300', 'class']]
+x_train = sensor_data_train.drop(['time', 'class'], axis=1)
+y_train = sensor_data_train['class']
 
-# sensor_data = sensor_data[['time', 'acc_z', 'acc_y', 'acc_x',
-#                   'gra_z', 'gra_y', 'gra_x',
-#                   'gyr_z', 'gyr_y', 'gyr_x',
-#                   'mag_z', 'mag_y', 'mag_x',
-#                   'mic_dBFS',
-#                   'ori_qz', 'ori_qy', 'ori_qx', 'ori_qw', 'class']]
-
-x = sensor_data.drop(['time', 'class'], axis=1)
-y = sensor_data['class']
+x_test = sensor_data_test.drop(['time', 'class'], axis=1)
+y_test = sensor_data_test['class']
 
 # Convert the target variable to numerical labels if needed
-y = pd.factorize(y)[0]
+y_train = pd.factorize(y_train)[0]
+y_test = pd.factorize(y_test)[0]
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+# Reshape data for LSTM Layer
+x_train = np.array(x_train).reshape((x_train.shape[0], x_train.shape[1], 1))
+x_test = np.array(x_test).reshape((x_test.shape[0], x_test.shape[1], 1))
 
-x_train = pad_sequences(x_train.values, padding='post')
-x_test = pad_sequences(x_test.values, padding='post')
-
-vocabulary_size = len(np.unique(x_train))
 embedding_dim = 200
 lstm_units = 128
-num_classes = len(np.unique(y))
-num_epochs = 100
-batch_size = 1024
+num_classes = len(np.unique(np.concatenate([y_train, y_test])))
+num_epochs = 10
+batch_size = 128
 
 # Define the LSTM model
 model = keras.Sequential()
-model.add(Embedding(input_dim=vocabulary_size, output_dim=embedding_dim, input_length=x_train.shape[1]))
-model.add(LSTM(units=lstm_units))
-model.add(Dense(units=num_classes, activation='softmax'))
+# model.add(LSTM(units=lstm_units, input_shape=(x_train.shape[1], 1)))
+# model.add(Dense(units=num_classes, activation='softmax'))
+
+model.add(LSTM(units=lstm_units, input_shape=(x_train.shape[1], 1), dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(units=num_classes, activation='softmax', kernel_regularizer=keras.regularizers.l1(0.01)))
+
+optimizer = keras.optimizers.Adam(learning_rate=0.0001)  # Default is 0.01, try smaller values
 
 model.compile(
-    optimizer='adam',
+    optimizer=optimizer,
     loss='sparse_categorical_crossentropy',
     metrics=['accuracy']
 )
@@ -66,6 +56,14 @@ history = model.fit(x_train, y_train, epochs=num_epochs, batch_size=batch_size, 
 
 test_loss, test_acc = model.evaluate(x_test, y_test)
 
+model.save("my_model.h5")
+
+hist_df = pd.DataFrame(history.history)
+# Save to csv
+hist_csv_file = 'history.csv'
+with open(hist_csv_file, mode='w') as f:
+    hist_df.to_csv(f)
+
 plt.plot(history.history['loss'])
 plt.title('Model Loss')
 plt.ylabel('Loss')
@@ -73,11 +71,9 @@ plt.xlabel('Epoch')
 plt.show()
 
 
-# Generate predictions
 y_pred = model.predict(x_test)
 y_pred = np.argmax(y_pred, axis=1)
 
-# Create confusion matrix
 cm = confusion_matrix(y_test, y_pred)
 
 mpl.rcParams['font.size'] = 15
@@ -87,3 +83,7 @@ plt.title('Confusion Matrix')
 plt.xlabel('Predicted Labels')
 plt.ylabel('True Labels')
 plt.show()
+
+
+print(test_loss)
+print(test_acc)
