b4973f4faf13b0a9652c9653f5cfff0a000f0ca1
zhiheng-yang <young-z-h@outlook.com>
2023-06-25 12:09:15 +0200

    lstm wrong

--- ch8_classification_lstm.py
+++ ch8_classification_lstm.py
@@ -9,10 +9,23 @@ import seaborn as sns
 import matplotlib as mpl
 
 target = 'class'
-# sensor_data_train = pd.read_csv('/Users/young/Downloads/ml4qs_codes/ML4QS_66/datasets/intermediate/bugfixed/chapter4_result_train_full.csv')
-# sensor_data_test = pd.read_csv('/Users/young/Downloads/ml4qs_codes/ML4QS_66/datasets/intermediate/bugfixed/chapter4_result_test_full.csv')
-sensor_data_train = pd.read_csv('/Users/young/Downloads/ml4qs_codes/ML4QS_66/datasets/intermediate/bugfixed/chapter4_result_train.csv')
-sensor_data_test = pd.read_csv('/Users/young/Downloads/ml4qs_codes/ML4QS_66/datasets/intermediate/bugfixed/chapter4_result_test.csv')
+cols = ['time','gra_y_min', 'gyr_x_diff_2_temp_std_ws_300', 'mag_z_temp_std_ws_300',
+                              'gra_y_median_temp_std_ws_300', 'mag_y_diff_2_temp_std_ws_300',
+                              'gra_y_autocorr_mean_temp_std_ws_300', 'ori_qz_fft_min_temp_mean_ws_300',
+                              'acc_z_fft_std_temp_std_ws_300', 'mag_z_max_temp_mean_ws_300',
+                              'gra_y_autocorr_median_temp_std_ws_300', 'acc_y_fft_max', 'acc_y_std',
+                              'mag_x_autocorr_max_temp_std_ws_300', 'gra_y', 'mag_x_skew_temp_std_ws_300',
+                              'gra_y_median', 'gyr_y_autocorr_median_temp_mean_ws_300', 'gyr_z_std_temp_mean_ws_300',
+                              'mic_dBFS', 'ori_qz_min_temp_mean_ws_300', 'class']
+sensor_data_train = pd.read_csv('/Users/young/Downloads/ml4qs_codes/ML4QS_66/datasets/intermediate/bugfixed/chapter4_result_train_full.csv')[cols]
+sensor_data_test = pd.read_csv('/Users/young/Downloads/ml4qs_codes/ML4QS_66/datasets/intermediate/bugfixed/chapter4_result_test_full.csv')[cols]
+# sensor_data_train = pd.read_csv('/Users/young/Downloads/ml4qs_codes/ML4QS_66/datasets/intermediate/bugfixed/chapter4_result_train.csv')[cols]
+# sensor_data_test = pd.read_csv('/Users/young/Downloads/ml4qs_codes/ML4QS_66/datasets/intermediate/bugfixed/chapter4_result_test.csv')[cols]
+
+
+sensor_data_train = sensor_data_train.sample(frac=1).reset_index(drop=True)
+sensor_data_test = sensor_data_test.sample(frac=1).reset_index(drop=True)
+
 
 x_train = sensor_data_train.drop(['time', 'class'], axis=1)
 y_train = sensor_data_train['class']
@@ -31,8 +44,8 @@ x_test = np.array(x_test).reshape((x_test.shape[0], x_test.shape[1], 1))
 embedding_dim = 200
 lstm_units = 128
 num_classes = len(np.unique(np.concatenate([y_train, y_test])))
-num_epochs = 10
-batch_size = 128
+num_epochs = 100
+batch_size = 1024
 
 # Define the LSTM model
 model = keras.Sequential()
@@ -52,9 +65,8 @@ model.compile(
 
 model.summary()
 
-print('start')
+history = model.fit(x_train, y_train, epochs=num_epochs, batch_size=batch_size, validation_data=(x_test, y_test))
 history = model.fit(x_train, y_train, epochs=num_epochs, batch_size=batch_size, validation_data=(x_test, y_test), verbose=1)
-print('end')
 
 test_loss, test_acc = model.evaluate(x_test, y_test)
 
